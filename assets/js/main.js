708a5fdb03d86de8694e45d11d97938e67dd5b7a
truongduy5998 <truongduy5998@gmail.com>
2019-03-13 20:39:13 +0700

    Add Homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,12 +23,29 @@ jQuery(function($) {
 		}
 	};
 
+	vnBuilding.partner__list = function() {
+		if( $('.partner__list').length ) {
+			$('.partner__list').slick({
+				infinite: true,
+				slidesToShow: 6,
+				slidesToScroll: 1,
+				// dots: true,
+				arrows: true,
+				// autoplay: true,
+				// autoplaySpeed: 4000,
+				prevArrow: `<div class="prev"><i class="icons flaticon-back"></i></div>`,
+				nextArrow: `<div class="next"><i class="icons flaticon-next"></i></div>`
+			});
+		}
+	};
+
 	/*==========================
 	=		INIT FUNCTION      =
 	============================*/
 
 	$(document).ready(function() {
 		vnBuilding.slider();
+		vnBuilding.partner__list();
 	});
 
 
